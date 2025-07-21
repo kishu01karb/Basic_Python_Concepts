d3517155aa68d05c9081ec2e0e4d254c2fae9977
kishu01karb <166857376+kishu01karb@users.noreply.github.com>
2025-07-21 16:29:03 +0530

    Add files via upload

---
+++ Task_1_Perform_Basic_Mathematical_Operations.py
@@ -0,0 +1,19 @@
+#Task1
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+num1= float(input("enter the value of number 1 : "))
+num2= float(input("enter the value of number 2 : "))
+#2.  Performs the basic mathematical operations on these two numbers:
+#o	Addition
+add = num1+num2
+print("Addiition of Two numbers are: ",add)
+#o	Subtraction
+sub = num1-num2
+print("Subtraction of Two numbers are: ", sub)
+#o	Multiplication
+mul = num1*num2
+print("Multiplication of Two numbers are : ",mul)
+#o	Division
+div = num1/num2
+print("Divison of Two numbers are : ",div)
+#3.  Displays the results of each operation on the screen.
---
+++ Task_2_Create_a_personalised_Greeting.py
@@ -0,0 +1,9 @@
+# Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+first_name = str(input("enter your first name: "))
+last_name = str(input("enter your last name: "))
+#2.  Concatenates the first name and last name into a full name.
+full_name = first_name +" " + last_name
+print(f"Hello, {full_name}!!! Welcome to the python world.")
+#3.  Prints a personalized greeting message using the full name.
+
